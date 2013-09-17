81265402e5ca64447fad3fb4a54a168de8381711
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-09-17 14:17:59 +0000

    added the functions to viewlist reservebook checkmembership status

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,40 +1,92 @@
 package com.twu.biblioteca;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Menu {
+    private static final String WELCOME_MESSAGE = "Welcome To The Bangalore Public Library";
+    private static final String OPTION_VIEW_LIST = "1. view a list of all the books in the library";
+    private static final String OPTION_RESERVE_A_BOOK = "2. reserve a book from collection";
+    private static final String OPTION_CHECK_MEMBERSHIP = "3. check your membership details";
 
-    public static final String welcome_Message = "Welcome To The Bangalore Public Library";
-    private static Scanner customer_Input = new Scanner(System.in);
-    private static String chosen_Option;
-    public static final String option_One = "1. view a list of all the books in the library";
-    public static final String option_Two = "2. reserve a book for collection";
-    public static final String option_Three = "3. check your membership details";
-    public static final String options = option_One + " " + option_Two + " "+ option_Three;
+    BufferedReader customerInput;
+    public final HashMap<String, String> options;
+    ArrayList<String> bookList;
 
 
-    public Menu() {
-        start();
+    public Menu() throws IOException {
+        bookList = new ArrayList<String>();
+        options = new HashMap<String, String>();
+        options.put("1", OPTION_VIEW_LIST);
+        options.put("2", OPTION_RESERVE_A_BOOK);
+        options.put("3", OPTION_CHECK_MEMBERSHIP);
+        bookList.add("Only Time Will Tell");
+        customerInput = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public static void main(String[] args) {
-        start();
+    public static void main(String[] args) throws IOException {
+        Menu menu = new Menu();
+        menu.start();
+        menu.selectMenu();
     }
 
-    public static void start() {
-        System.out.println(welcome_Message);
+    public void start() {
+        System.out.println(WELCOME_MESSAGE);
         displayOptions();
     }
 
-    private  static void options (){
-
-    }
-    public static void displayOptions() {
-
+    private void displayOptions() {
+        String menuItems = "";
         System.out.println("Main Menu:");
-        System.out.println(options);
+        for (String key : options.keySet()) {
+            menuItems = options.get(key) + " " + menuItems;
+        }
+        System.out.println(menuItems);
+    }
 
+    public void selectMenu() throws IOException {
 
+        System.out.println("enter the number of your desired option:");
+        String chosenOption = customerInput.readLine();
+        displayUserInputChoice(chosenOption);
+    }
+
+    private void displayUserInputChoice(String userInput) throws IOException {
+        if (!options.containsKey(userInput)) {
+            throw new IllegalArgumentException(" Select a valid option!!");
+        }
+        System.out.println("You want to:" + options.get(userInput));
+
+        switch (Integer.parseInt(userInput)) {
+            case 1:
+                viewBooks();
+                break;
+            case 2:
+                reserveBook();
+                break;
+            case 3:
+                checkMembershipDetails();
+                break;
+        }
+    }
+
+    public void reserveBook() throws IOException {
+        System.out.println("enter the title of the book you want to reserve:");
+        String chosenBook = customerInput.readLine();
+        if (bookList.contains(chosenBook)) System.out.println("Thank You! Enjoy the book.");
+        else throw new IllegalArgumentException("Sorry we don't have that book yet");
+    }
+
+    public void checkMembershipDetails() {
+        System.out.println("Please talk to a Librarian. Thank you.");
+    }
+
+    public void viewBooks() {
+        System.out.println("Below is a list of all the books in the Bangalore library:");
+        System.out.println(bookList);
     }
 }
 
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -5,7 +5,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
@@ -20,28 +22,77 @@ public class MenuTest {
         System.setOut(new PrintStream(outContent));
     }
 
-    @Test
-    public void shouldDisplayWelcomeMessageWhenApplicationIsStarted() {
-        Menu menu = new Menu();
-        assertThat(menu.welcome_Message, is(getContentLine(0)));
-
-    }
-      @Test
-      public void shouldSeeAListOfMenuOptionsAtTheStartOfTheApplication(){
-          Menu menu = new Menu();
-          assertThat(menu.options, is (getContentLine(2)))  ;
-      }
-
-    public String getContentLine(int num) {
-        String display []   = outContent.toString().split("\n")  ;
-      return display[num]  ;
-    }
-
-
-
     @After
     public void cleanUpStreams() {
         System.setOut(null);
+    }
 
+    @Test
+    public void shouldDisplayWelcomeMessageWhenApplicationIsStarted() throws IOException {
+        Menu menu = new Menu();
+        menu.start();
+        assertThat(getContentLine(1), is("Welcome To The Bangalore Public Library"));
+    }
+
+    @Test
+    public void shouldSeeAListOfMenuOptionsAtTheStartOfTheApplication() throws IOException {
+        Menu menu = new Menu();
+        menu.start();
+        assertThat(getContentLine(3), is("1. view a list of all the books in the library " +
+                "2. reserve a book from collection " +
+                "3. check your membership details "));
+    }
+
+    @Test
+    public void shouldAllowCustomerToSelectMenuOptions() throws IOException {
+        ByteArrayInputStream inContent = new ByteArrayInputStream("1".getBytes());
+        System.setIn(inContent);
+        Menu menu = new Menu();
+        menu.selectMenu();
+        assertThat(getContentLine(2), is("You want to:" + menu.options.get("1")));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotifyCustomerIfAnInvalidMenuOptionIsSelected() throws IOException {
+        ByteArrayInputStream inContent = new ByteArrayInputStream("m".getBytes());
+        System.setIn(inContent);
+        Menu menu = new Menu();
+        menu.selectMenu();
+    }
+
+    @Test
+    public void shouldViewAListOfAllBooksWhenTheViewBooksOptionIsSelected() throws IOException {
+        Menu menu = new Menu();
+        menu.viewBooks();
+        assertThat(getContentLine(2), is("[Only Time Will Tell]"));
+    }
+
+    @Test
+    public void shouldBeAbleToCheckMembershipDetails() throws IOException {
+        Menu menu = new Menu();
+        menu.checkMembershipDetails();
+        assertThat(getContentLine(1), is("Please talk to a Librarian. Thank you."));
+    }
+
+    @Test
+    public void shouldBeAbleToReserveABookForCollection() throws IOException {
+        ByteArrayInputStream inContent = new ByteArrayInputStream("Only Time Will Tell".getBytes());
+        System.setIn(inContent);
+        Menu menu = new Menu();
+        menu.reserveBook();
+        assertThat(getContentLine(2), is("Thank You! Enjoy the book."));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void shouldNotifyCustomerIfSelectedBookIsNotAvailable() throws IOException {
+        ByteArrayInputStream inContent = new ByteArrayInputStream("This Book Is Not Available".getBytes());
+        System.setIn(inContent);
+        Menu menu = new Menu();
+        menu.reserveBook();
+    }
+
+    public String getContentLine(int num) {
+        String display[] = outContent.toString().split("\n");
+        return display[num - 1];
     }
 }
