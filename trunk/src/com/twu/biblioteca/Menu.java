7e325c4855a574bc4381cc2f714d07765e6bc636
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-09-18 08:02:56 +0000

    modified the case statements to take meaningfull variables, deleted the start method and moved everything to the displaymenuoptions method

--- trunk/src/com/twu/biblioteca/Menu.java
+++ trunk/src/com/twu/biblioteca/Menu.java
@@ -11,11 +11,9 @@ public class Menu {
     private static final String OPTION_VIEW_LIST = "1. view a list of all the books in the library";
     private static final String OPTION_RESERVE_A_BOOK = "2. reserve a book from collection";
     private static final String OPTION_CHECK_MEMBERSHIP = "3. check your membership details";
-
-    BufferedReader customerInput;
+    private BufferedReader customerInput;
     public final HashMap<String, String> options;
-    ArrayList<String> bookList;
-
+    private ArrayList<String> bookList;
 
     public Menu() throws IOException {
         bookList = new ArrayList<String>();
@@ -24,31 +22,27 @@ public class Menu {
         options.put("2", OPTION_RESERVE_A_BOOK);
         options.put("3", OPTION_CHECK_MEMBERSHIP);
         bookList.add("Only Time Will Tell");
+        bookList.add("Cain and Abel");
         customerInput = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
-        menu.start();
+        menu.displayMenuItems();
         menu.selectMenu();
     }
 
-    public void start() {
+    public void displayMenuItems() {
         System.out.println(WELCOME_MESSAGE);
-        displayMenuItems();
-    }
-
-    private void displayMenuItems() {
         String menuItems = "";
         System.out.println("Main Menu:");
-        for (String key : options.keySet()) {
-            menuItems = options.get(key) + " " + menuItems;
+        for (String option : options.values()) {
+            menuItems = option + " " + menuItems;
         }
         System.out.println(menuItems);
     }
 
     public void selectMenu() throws IOException {
-
         System.out.println("enter the number of your desired option:");
         String chosenOption = customerInput.readLine();
         displayUserInputChoice(chosenOption);
@@ -59,15 +53,17 @@ public class Menu {
             throw new IllegalArgumentException(" Select a valid option!!");
         }
         System.out.println("You want to:" + options.get(userInput));
-
+        final int itemOne = 1;
+        final int itemTwo = 2;
+        final int itemThree = 3;
         switch (Integer.parseInt(userInput)) {
-            case 1:
+            case itemOne:
                 viewBooks();
                 break;
-            case 2:
+            case itemTwo:
                 reserveBook();
                 break;
-            case 3:
+            case itemThree:
                 checkMembershipDetails();
                 break;
         }
@@ -76,8 +72,11 @@ public class Menu {
     public void reserveBook() throws IOException {
         System.out.println("enter the title of the book you want to reserve:");
         String chosenBook = customerInput.readLine();
-        if (bookList.contains(chosenBook)) System.out.println("Thank You! Enjoy the book.");
-        else throw new IllegalArgumentException("Sorry we don't have that book yet");
+        if (bookList.contains(chosenBook)) {
+            System.out.println("Thank You! Enjoy the book.");
+        } else {
+            throw new IllegalArgumentException("Sorry we don't have that book yet");
+        }
     }
 
     public void checkMembershipDetails() {
