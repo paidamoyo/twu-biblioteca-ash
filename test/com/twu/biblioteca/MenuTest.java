fa91262ded2c86bce13da450e1c90891a9db055d
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-09-19 12:03:14 +0000

    created a consoler help class for all the methods shared by the all the test class

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,30 +1,14 @@
 package com.twu.biblioteca;
 
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class MenuTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
+public class MenuTest extends ConsoleHelperTest {
 
     @Test
     public void shouldDisplayWelcomeMessageWhenApplicationIsStarted() throws IOException {
@@ -45,14 +29,16 @@ public class MenuTest {
 
     @Test
     public void shouldAllowCustomerToSelectMenuOptions() throws IOException {
-        Menu menu =simulateUserInputIntoConsole("1");
+        simulateUserInputIntoConsole("1");
+        Menu menu = new Menu();
         menu.selectMenu();
         assertThat(getContentLine(2), is("You want to:" + menu.options.get("1")));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotifyCustomerIfAnInvalidMenuOptionIsSelected() throws IOException {
-        Menu menu = simulateUserInputIntoConsole("m");
+        simulateUserInputIntoConsole("m");
+        Menu menu = new Menu();
         menu.selectMenu();
     }
 
@@ -62,10 +48,11 @@ public class MenuTest {
         menu.checkMembershipDetails();
         assertThat(getContentLine(1), is("Please talk to a Librarian. Thank you."));
     }
+
     @Test
     public void shouldBeAbleToReserveABookForCollection() throws IOException {
         simulateUserInputIntoConsole("Only Time Will Tell");
-       Menu menu = new Menu();
+        Menu menu = new Menu();
         menu.reserveBook();
         assertThat(getContentLine(2), is("Thank You! Enjoy the book."));
     }
@@ -75,16 +62,5 @@ public class MenuTest {
         simulateUserInputIntoConsole("This Book Is Not Available");
         Menu menu = new Menu();
         menu.reserveBook();
-    }
-
-    public Menu simulateUserInputIntoConsole(String userInput) throws IOException {
-        ByteArrayInputStream inContent = new ByteArrayInputStream(userInput.getBytes());
-        System.setIn(inContent);
-        return new Menu();
-    }
-
-    public String getContentLine(int num) {
-        String display[] = outContent.toString().split("\n");
-        return display[num - 1];
     }
 }
