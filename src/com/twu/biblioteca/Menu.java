82412afe8646e1391efc550c46f94c3be404628c
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-09-25 12:24:14 +0000

    add diplay menu options after execution of each menu item

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -15,7 +15,7 @@ public class Menu {
     private static final int RESERVE_A_BOOK_CHOICE = 2;
     private static final int CHECK_MEMBERSHIP_CHOICE = 3;
     private static final int VIEW_MOVIE_LIST_CHOICE = 4;
-    private boolean successfullyLoggedIn = false;
+    private boolean successfullyLoggedIn;
 
     private BufferedReader customerInput;
     public final HashMap<String, String> options;
@@ -34,20 +34,21 @@ public class Menu {
         movie = new Movies();
         books = new Books();
         userAccounts = new UserAccounts();
+        successfullyLoggedIn = false;
     }
 
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
         menu.displayMenuItems();
-        menu.selectMenu();
     }
 
-    public void displayMenuItems() {
+    public void displayMenuItems() throws IOException {
         System.out.println(WELCOME_MESSAGE);
         System.out.println("Main Menu:");
         for (String option : options.values()) {
             System.out.println(option);
         }
+        selectMenu();
     }
 
     public void selectMenu() throws IOException {
@@ -81,29 +82,32 @@ public class Menu {
         System.out.println("enter the title of the book you want to reserve:");
         String chosenBook = customerInput.readLine();
         books.processReservation(chosenBook);
+       displayMenuItems();
     }
 
     public void checkMembershipDetails() throws IOException {
-       if (successfullyLoggedIn) {
-           userAccounts.displayMembershipDetails(currentUser);
-       }
-        else {
-           System.out.println("Please talk to a Librarian. Thank you.");
-       }
+        if (successfullyLoggedIn) {
+            userAccounts.displayMembershipDetails(currentUser);
+        } else {
+            System.out.println("Please talk to a Librarian. Thank you.");
+        }
+       displayMenuItems();
     }
 
-    public void viewBooks() {
+    public void viewBooks() throws IOException {
         System.out.println("Below is a list of all the books in the Bangalore library:");
         books.display();
+        displayMenuItems();
     }
 
     public void viewMovies() throws IOException {
         login();
         movie.display();
+        displayMenuItems();
 
     }
 
-    public void login() throws IOException {
+    private void login() throws IOException {
         if (!successfullyLoggedIn) {
             System.out.println("Enter your username:");
             String username = customerInput.readLine();
