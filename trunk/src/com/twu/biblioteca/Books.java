60edd0271e90b7a39d4145f2f3dfa259d6642ccc
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-10-01 14:49:10 +0000

    independency injection of system.out in the books classes

--- trunk/src/com/twu/biblioteca/Books.java
+++ trunk/src/com/twu/biblioteca/Books.java
@@ -1,25 +1,28 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Books {
     private ArrayList<String> bookList;
+    private PrintStream printStream;
 
-    public Books() {
+    public Books(PrintStream printStream) {
+        this.printStream = printStream;
         bookList = new ArrayList<String>();
         bookList.add("Only Time Will Tell");
         bookList.add("Cain and Abel");
     }
 
     public void display() {
-        System.out.println(bookList);
+        printStream.println(bookList.toString());
     }
 
     public void processReservation(String bookName) {
         if (bookList.contains(bookName)) {
-            System.out.println("Thank You! Enjoy the book.");
+            printStream.println("Thank You! Enjoy the book.");
         } else {
-            System.out.println("Sorry we don't have that book yet");
+            printStream.println("Sorry we don't have that book yet");
         }
     }
 }
