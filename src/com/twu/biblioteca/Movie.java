f7c582c46dead58b6853517f4cd2339c002f8780
ashgirlchapfuwa@gmail.com <ashgirlchapfuwa@gmail.com@4eefa556-67f1-98d3-0329-26c327d024d9>
2013-10-01 18:58:06 +0000

    deleted consolehelper test class and implemented dependency injection of bufferedreader and inputstream

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,6 +1,5 @@
 package com.twu.biblioteca;
 
-import java.io.PrintStream;
 
 public class Movie {
 
@@ -8,14 +7,12 @@ public class Movie {
     private String year;
     private String director;
     private String rating;
-    private PrintStream printStream;
 
     public Movie(String name, String year, String director, String rating) {
         this.name = name;
         this.year = year;
         this.director = director;
         this.rating = rating;
-        this.printStream = printStream;
     }
 
     @Override
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,21 +1,30 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
 
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
-public class MenuTest extends ConsoleHelperTest {
+public class MenuTest  {
+
+    private BufferedReader mockBufferedReader;
+    private PrintStream mockPrintStream;
+
+    @Before
+    public void setUp() {
+        mockBufferedReader = mock(BufferedReader.class);
+        mockPrintStream = mock(PrintStream.class);
+    }
 
     @Test
     public void shouldDisplayWelcomeMessageWhenApplicationIsStarted() throws IOException {
-        PrintStream mockPrintStream = mock(PrintStream.class);
-        Menu menu = new Menu(mockPrintStream);
+        Menu menu = new Menu(mockPrintStream, mockBufferedReader);
         menu.displayMenuItems();
         verify(mockPrintStream).println("Welcome To The Bangalore Public Library");
         verify(mockPrintStream).println("Main Menu:");
@@ -23,41 +32,40 @@ public class MenuTest extends ConsoleHelperTest {
 
     @Test
     public void shouldSeeAListOfMenuOptionsAtTheStartOfTheApplication() throws IOException {
-        PrintStream mockPrintStream = mock(PrintStream.class);
-        Menu menu = new Menu(mockPrintStream);
+        Menu menu = new Menu(mockPrintStream, mockBufferedReader);
         menu.displayMenuItems();
         verify(mockPrintStream).println("3. check your membership details");
     }
 
     @Test
     public void shouldAllowCustomerToSelectMenuOptions() throws IOException {
-        simulateUserInputIntoConsole("1");
+        when(mockBufferedReader.readLine()).thenReturn("1");
         PrintStream mockPrintStream = mock(PrintStream.class);
-        Menu menu = new Menu(mockPrintStream);
+        Menu menu = new Menu(mockPrintStream, mockBufferedReader);
         menu.selectMenu();
+        verify(mockPrintStream).println("enter the number of your desired option:");
         verify(mockPrintStream).println("You want to:1. view a list of all the books in the library");
     }
 
     @Test
     public void shouldNotifyCustomerIfAnInvalidMenuOptionIsSelected() throws IOException {
-        simulateUserInputIntoConsole("m");
+        when(mockBufferedReader.readLine()).thenReturn("m");
         PrintStream mockPrintStream = mock(PrintStream.class);
-        Menu menu = new Menu(mockPrintStream);
+        Menu menu = new Menu(mockPrintStream, mockBufferedReader);
         menu.selectMenu();
         verify(mockPrintStream).println(" Select a valid option!!");
     }
 
     @Test
     public void shouldBeAbleToCheckMembershipDetails() throws IOException {
-        PrintStream mockPrintStream = mock(PrintStream.class);
-        Menu menu = new Menu(mockPrintStream);
+        Menu menu = new Menu(mockPrintStream, mockBufferedReader);
         menu.checkMembershipDetails();
         verify(mockPrintStream).println("Please talk to a Librarian. Thank you.");
     }
 
     @Test
     public void shouldNotifyTheUserIfTheProgramIsQuit() throws IOException {
-        Menu menu = new Menu(System.out);
+        Menu menu = new Menu(System.out, mockBufferedReader);
         menu.quit();
         assertTrue(menu.hasQuit());
     }
